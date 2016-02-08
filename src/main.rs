57718bcd599ca6bf6a800414c5086812ea1ce666
Thibault Riviere <thibault.riviere@scality.com>
2016-02-09 00:41:00 +0100

    [db](add)  add db wrapper for levelDb
    Lot of information needs to be persisants
    And need a database for organise them .

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
+extern crate leveldb;
+
+mod db;
 mod state;
 
 use state::State;
+use db::Db;
 
 fn main() {
-    State::new_with_value(3, 3, 4);
+    let mut test_state = State::new();
+    println!("create state : test commit 1 = {}", test_state.commit(1));
+    let path = "/tmp/test";
+    {
+        let db = Db::new(&path, true);
+        match db {
+            Ok(_) => {
+                println!("Database succefully created");
+            }, Err(e) => {
+                println!("Err creating Database err: {}", e);
+            }
+        }
+    }
+    Db::destroy(::std::path::Path::new("/tmp/test")).unwrap();
 }
